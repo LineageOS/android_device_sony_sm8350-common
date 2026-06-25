8a4df77d6dc80c1e760432dbd625c4fab0d7cff6
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:09:48 -0400

    sm8350-common: Use Clang r563880c for kernel build
    * Doesn't boot with default Clang version.
    
    Change-Id: I35765e296044eef428d01bbf5a21e91f1897704e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,6 +80,7 @@ BOARD_DTB_OFFSET           := 0x01f00000
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_VERSION := r563880c
 TARGET_KERNEL_CONFIG := \
     vendor/lahaina-qgki_defconfig \
     vendor/sagami.config
